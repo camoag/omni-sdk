df08b699d62d9dfa8bf6cba3fb8cdc539bf8dd24
Danny Tiesling <tiesling@gmail.com>
2024-07-02 09:20:40 -0700

    Update src/omni/embed.py

--- src/omni/embed.py
+++ src/omni/embed.py
@@ -77,7 +77,7 @@ class OmniDashboardEmbedder:
             or f"{omni_config.organization_name}.embed-omniapp.co"
         )
         self.embed_login_url = f"https://{embed_host}/embed/login"
-        assert omni_config.embed_secret  # Required to appease mypy.
+        assert omni_config.embed_secret  # Required to appease mypy. If embed_secret is missing an OmniConfigError will have already been raised by the OmniConfig class.
         self.embed_secret = omni_config.embed_secret
 
     def build_url(
